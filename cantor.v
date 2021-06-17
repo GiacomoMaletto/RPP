e9052398b0f7d715982a967f3fd2204e011396c9
GiacomoMaletto <malettogiacomo6@gmail.com>
2021-06-17 23:01:45 +0200

    Update cantor.v

--- cantor.v
+++ cantor.v
@@ -2,14 +2,6 @@ Require Import Unicode.Utf8 List ZArith.
 Import ListNotations.
 Set Implicit Arguments.
 
-Fixpoint iter X (f : X → X) (n : nat) x :=
-  match n with
-  | 0 => x
-  | S n => f (iter f n x)
-  end.
-
-Module RPP.
-
 Inductive RPP :=
   | Id (n : nat)
   | Ne
@@ -49,6 +41,12 @@ Fixpoint arity f :=
   | Pa f g => arity f + arity g
   | It f => S (arity f)
   | If f g h => S (max (arity f) (max (arity g) (arity h)))
+  end.
+
+Fixpoint iter X (f : X → X) (n : nat) x :=
+  match n with
+  | 0 => x
+  | S n => f (iter f n x)
   end.
 
 Open Scope Z_scope.
@@ -144,57 +142,95 @@ Compute «cu» [83;0;0;0].
 Compute «cp» [5;7;0;0].
 Compute «push» [5;7;0;0].
 Compute «pop» [83;0;0;0].
-Compute push.
-
-End RPP.
-
-Module PRF.
 
 Open Scope nat_scope.
 
 Inductive PRF :=
-  | Ze : nat → PRF
-  | Su : nat → PRF
-  | Pr : nat → PRF
-  | Co : PRF → PRF → PRF
-  | Pa : PRF → PRF → PRF
-  | Re : PRF → PRF → PRF.
+  | ZE : nat → PRF
+  | SU : nat → nat → PRF
+  | PR : nat → nat → PRF
+  | CO : PRF → list PRF → PRF
+  | RE : PRF → PRF → PRF.
 
-Fixpoint arity (f : PRF) :=
-  match f with
-  | Ze n | Su n | Pr n => n
-  | Co f g => max (arity f) (arity g)
-  | Pa f g => arity f + arity g
-  | Re f g => S (arity f)
+Fixpoint ARITY (F : PRF) : nat :=
+  match F with
+  | ZE n | SU _ n | PR _ n => n
+  | CO F Gs => list_max (map ARITY Gs)
+  | RE F G => max (S (ARITY F)) (pred (ARITY G))
   end.
-Definition miao : nat*nat*nat := (3,1,2).
-Definition bau (p : nat*nat*nat) : nat*nat*nat :=
-  match p with (a, b, c) => (3,2,1)
-  end.
-Fixpoint evaluate f (l : list nat) : list nat :=
-  match f with
-  | Ze n => [O]
-  | Su n => [S (nth (pred n) l O)]
-  | Pr n => [(nth (pred n) l O)]
-  | Co f g => (evaluate f) (evaluate g l)
-  | Pa f g => let n := arity f in
-    evaluate f (firstn n l) ++ evaluate g (skipn n l)
-  | Re f g => match l with []=>[]
+
+Fixpoint EVALUATE F (l : list nat) : nat :=
+  match F with
+  | ZE n => O
+  | SU i n => S (nth (pred i) l O)
+  | PR i n => (nth (pred i) l O)
+  | CO F Gs => EVALUATE F (map (λ G, EVALUATE G l) Gs)
+  | RE F G => match l with []=>0
     | n::l' => fst (fst (iter
-      (λ p, match p with (a,x,y) => (evaluate g (x::a++y), S x, y) end)
+      (λ p, match p with (a,x,y) => (EVALUATE G (a::x::y), S x, y) end)
       n
-      (evaluate f l', O, l')))
+      (EVALUATE F l', 0, l')))
     end
   end.
 
-Definition add := Re (Pr 1) (Co (Su 1) (Pr 2)).
-Definition pre := Re (Ze 1) (Pr 1).
-Definition sub := Re (Pr 1) (Co pre (Pr 2)).
-Definition pro := Re (Ze 1) (Co add (Pa (Pr 2) (Pr 1))).
-Compute evaluate add [4;4].
-Compute evaluate pre [4].
-Compute evaluate sub [3;5].
-Compute evaluate pro [44;313].
+Definition ADD := RE (PR 1 1) (CO (SU 1 1) [PR 1 3]).
+Definition PRE := RE (ZE 0) (PR 2 3).
+Definition SUB := RE (PR 1 1) (CO PRE [PR 1 3]).
+Definition MUL := RE (ZE 1) (CO ADD [PR 1 3;PR 3 3]).
+Compute EVALUATE ADD [4;4].
+Compute EVALUATE PRE [4].
+Compute EVALUATE SUB [3;5].
+Compute EVALUATE MUL [44;313].
+
+Fixpoint co_loading n m gs :=
+  match gs with
+  | [] => Id 0
+  | g::gs' => Id n ‖ (\[m]\ ;; g) ;; co_loading (S n) m gs'
+  end.
+
+Fixpoint convert (F : PRF) : RPP :=
+  match F with
+  | ZE n => Id n
+  | SU i n => Su ;; \[1+i;1]\ ;; inc ;; inv(\[1+i;1]\) ‖ Id (n - i)
+  | PR i n => \[1+i;1]\ ;; inc ;; inv(\[1+i;1]\) ‖ Id (n - i)
+  | CO F Gs => let (n, m) := (ARITY (CO F Gs), length Gs) in
+    co_loading 1 (1+n) (map convert Gs) ;;
+    \seq (2+m) n\ ;;
+    Id n ‖ (convert F) ;;
+    inv (co_loading 1 (1+n) (map convert Gs) ;;
+    \seq (2+m) n\)
+  | RE F G => let n := pred (ARITY (RE F G)) in
+    Id 2 ‖ \seq (1+n) 6\ ;;
+    Id 7 ‖ convert F ;;
+    Id 6 ‖ Sw ;;
+    Id 1 ‖ It (Id 3 ‖ (convert G ;; Sw) ;; \[1;4]\ ;; push ;; Id 5 ‖ Su) ;;
+    \[7;1]\ ;;
+    inc ;;
+    inv (Id 2 ‖ \seq (1+n) 6\ ;;
+    Id 7 ‖ convert F ;;
+    Id 6 ‖ Sw ;;
+    Id 1 ‖ It (Id 3 ‖ (convert G ;; Sw) ;; \[1;4]\ ;; push ;; Id 5 ‖ Su) ;;
+    \[7;1]\)
+  end.
+
+Definition pad f l := evaluate f (l ++ repeat 0%Z (arity f - length l)).
+
+Compute pad (convert PRE) [0;5]%Z.
+Compute pad (convert ADD) [0;3;4]%Z.
+Compute pad (convert SUB) [0;2;5]%Z.
+Compute pad (convert MUL) [0;2;2]%Z.
+
+Fixpoint count f : nat :=
+  match f with
+  | Id _ | Ne | Su | Pr | Sw => 1
+  | Co f g => count f + count g
+  | Pa f g => count f + count g
+  | It f => 1 + count f
+  | If f g h => 1 + count f + count g + count h
+  end.
+
+Compute count (convert ADD).
+
 
 
 
